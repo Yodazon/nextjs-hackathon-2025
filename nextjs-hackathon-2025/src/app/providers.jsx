f0e0cdcd7484ed77b0408e2fa27bf40653651ef7
Yodazon <michael.karas96@gmail.com>
2025-04-15 20:46:24 -0400

    removed pointless convex

--- nextjs-hackathon-2025/src/app/providers.jsx
+++ nextjs-hackathon-2025/src/app/providers.jsx
@@ -1,12 +1,6 @@
 "use client";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { SessionProvider } from "next-auth/react";
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export function Providers({ children }) {
-  return (
-    <SessionProvider>
-      <ConvexProvider client={convex}>{children}</ConvexProvider>
-    </SessionProvider>
-  );
+  return <SessionProvider>{children}</SessionProvider>;
 }
